5ba78da17141520009c56b1e5ee0aa84689f6e38
Stepan Onysko <onysko.oleh.stepan.work@gmail.com>
2025-01-12 16:41:38 +0200

    added gson converter to RetrofitInstance.kt

--- app/src/main/java/com/example/retrofitexample/retrofit/RetrofitInstance.kt
+++ app/src/main/java/com/example/retrofitexample/retrofit/RetrofitInstance.kt
@@ -1,34 +1,38 @@
 package com.example.retrofitexample.retrofit
 
 import com.example.retrofitexample.retrofit.util.Constants.Companion.BASE_URL
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
-class RetrofitInstance {
-    companion object {
-        private val loggingInterceptor: HttpLoggingInterceptor by lazy {
-            HttpLoggingInterceptor()
-                .setLevel(HttpLoggingInterceptor.Level.BODY)
-        }
+object RetrofitInstance {
+    private val loggingInterceptor: HttpLoggingInterceptor by lazy {
+        HttpLoggingInterceptor()
+            .setLevel(HttpLoggingInterceptor.Level.BODY)
+    }
 
-        private val okHttpClient: OkHttpClient by lazy {
-            OkHttpClient.Builder()
-                .addInterceptor(loggingInterceptor)
-                .build()
-        }
+    private val okHttpClient: OkHttpClient by lazy {
+        OkHttpClient.Builder()
+            .addInterceptor(loggingInterceptor)
+            .build()
+    }
 
-        private val retrofit: Retrofit by lazy {
-            Retrofit.Builder()
-                .baseUrl(BASE_URL)
-                .addConverterFactory(GsonConverterFactory.create())
-                .client(okHttpClient)
-                .build()
-        }
+    private val gson: Gson by lazy {
+        GsonBuilder().setLenient().create()
+    }
 
-        val api: ApiService by lazy {
-            retrofit.create(ApiService::class.java)
-        }
+    private val retrofit: Retrofit by lazy {
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create(gson))
+            .client(okHttpClient)
+            .build()
+    }
+
+    val api: ApiService by lazy {
+        retrofit.create(ApiService::class.java)
     }
 }
